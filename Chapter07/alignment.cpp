aae4f0d3f994354150a99af1b87fc0bf67ce4234
BjornAndrist <bjorn.andrist@gmail.com>
2020-08-06 22:20:30 +0200

    Chapter 7, Memory management

---
+++ Chapter07/alignment.cpp
@@ -0,0 +1,62 @@
+#include <bit>
+#include <gtest/gtest.h>
+#include <iostream>
+
+bool is_aligned(void* ptr, std::size_t alignment) {
+  assert(ptr != nullptr);
+  // assert(std::has_single_bit(alignment)); // Power of 2   // [ Requires C++20
+  // ]
+
+  auto s = std::numeric_limits<std::size_t>::max();
+  // void* aligned_ptr = ptr;
+  auto* aligned_ptr = ptr;
+  std::align(alignment, 1, aligned_ptr, s);
+  return ptr == aligned_ptr;
+}
+
+// Memory returned from new is correctly aligned for std::max_align_t
+// which means that it is also correctly aligned for any scalar type
+TEST(Alignment, PointerAddress) {
+  auto* p = new char{};
+  auto address = reinterpret_cast<std::uintptr_t>(p);
+  auto max_alignment = alignof(std::max_align_t);
+  ASSERT_TRUE(is_aligned(p, max_alignment));
+}
+
+// Memory returned from new is correctly aligned for std::max_align_t
+// which means that it is also correctly aligned for any scalar type
+TEST(Alignment, SingleByteAllocation) {
+  auto* p1 = new char{};
+  auto* p2 = new char{};
+  auto a1 = reinterpret_cast<std::uintptr_t>(p1);
+  auto a2 = reinterpret_cast<std::uintptr_t>(p2);
+  std::cout << "Num bytes between p1 and p2: " << a2 - a1 << '\n';
+  // auto max_alignment = alignof(std::max_align_t);
+  // ASSERT_TRUE(is_aligned(p, max_alignment));
+}
+
+// A type with custom alignment
+struct alignas(64) CacheLine {
+  std::byte data[64];
+};
+
+// Custom aligned types
+TEST(Alignment, CacheLine) {
+  auto x = CacheLine{};
+  auto y = CacheLine{};
+  ASSERT_TRUE(is_aligned(&x, 64));
+  ASSERT_TRUE(is_aligned(&y, 64));
+
+  auto* p = new CacheLine{};
+  ASSERT_TRUE(is_aligned(p, 64));
+}
+
+// new with alignment
+TEST(Alignment, AlignedNew) {
+  constexpr auto ps = std::size_t{4096}; // Page size
+  auto n = ps / sizeof(int);
+  auto* page = new (std::align_val_t{ps}) int[n];
+  // page is now a memory page full of ints
+  ASSERT_TRUE(is_aligned(page, ps));
+  delete[] page;
+}
---
+++ Chapter07/polymorphic-memory-allocators.cpp
@@ -0,0 +1,63 @@
+#include <array>
+#include <iostream>
+#include <memory_resource>
+#include <set>
+
+#include <gtest/gtest.h>
+
+
+
+// Polymorphic memory allocators is a C++17 feature.
+// Clang doesn't support polymorphic memory allocators yet (mid 2020)
+
+
+// Using pmr containers
+TEST(PolymorphicMemoryAllocators, MonotonicBuffer) {
+  auto buffer = std::array<std::byte, 512>{};
+  auto resource = std::pmr::monotonic_buffer_resource{
+      buffer.data(), buffer.size(), std::pmr::new_delete_resource()};
+
+  auto unique_numbers = std::pmr::set<int>{&resource};
+
+  auto n = int{};
+  while (std::cin >> n) {
+    unique_numbers.insert(n);
+  }
+
+  for (const auto& number : unique_numbers) {
+    std::cout << number << '\n';
+  }
+}
+
+// A custom memory resource
+
+class PrintingResource : public std::pmr::memory_resource {
+public:
+  PrintingResource() : res_{std::pmr::get_default_resource()} {}
+
+private:
+  void* do_allocate(std::size_t bytes, std::size_t alignment) override {
+    std::cout << "allocate: " << bytes << '\n';
+    return res_->allocate(bytes, alignment);
+  }
+
+  void do_deallocate(void* p, std::size_t bytes,
+                     std::size_t alignment) override {
+    std::cout << "deallocate: " << bytes << '\n';
+    return res_->deallocate(p, bytes, alignment);
+  }
+
+  bool
+  do_is_equal(const std::pmr::memory_resource& other) const noexcept override {
+    return (this == &other);
+  }
+  std::pmr::memory_resource* res_;
+};
+
+TEST(PolymorphicMemoryAllocators, CustomMemoryResource) {
+  auto res = PrintingResource{};
+  auto vec = std::pmr::vector<int>{&res};
+
+  vec.emplace_back(1);
+  vec.emplace_back(2);
+}
