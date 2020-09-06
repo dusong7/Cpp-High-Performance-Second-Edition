917ca354813c3e2ee60e722da311570f3ba68e83
BjornAndrist <bjorn.andrist@gmail.com>
2020-09-06 23:12:08 +0200

    Compare short and long strings

--- Chapter10/benchmarks/string_concat_proxy_bm.cpp
+++ Chapter10/benchmarks/string_concat_proxy_bm.cpp
@@ -26,7 +26,8 @@ auto create_strings(int n, int length) {
 }
 
 template <typename T> void bm_string_compare(benchmark::State& state) {
-  const auto n = 10'000, length = 10;
+  const auto n = 10'000;
+  const auto length = state.range(0);
   const auto a = create_strings<T>(n, length);
   const auto b = create_strings<T>(n, length);
   const auto c = create_strings<T>(n, length * 2);
@@ -40,7 +41,7 @@ template <typename T> void bm_string_compare(benchmark::State& state) {
 
 } // namespace
 
-BENCHMARK_TEMPLATE(bm_string_compare, std::string);
-BENCHMARK_TEMPLATE(bm_string_compare, String);
+BENCHMARK_TEMPLATE(bm_string_compare, std::string)->Arg(50)->Arg(10)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(bm_string_compare, String)->Arg(50)->Arg(10)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
